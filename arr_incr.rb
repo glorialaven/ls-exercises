08e452b3a0dc9d306a46f5207f4531f53df1b3cd
Gloria Lavenberg <gloria@Glorias-MacBook-Air.local>
2016-06-22 02:11:35 -0400

    ...

---
+++ arr_incr.rb
@@ -0,0 +1,3 @@
+arr = [1, 2, 3, 4, 5, 6, 7, 8]
+arr.map {|x| puts x+2}
+p arr
---
+++ caps_string.rb
@@ -0,0 +1,9 @@
+def caps_string(str)
+  if str.length > 10
+    puts str.upcase
+  else
+    puts str
+  end
+end
+
+caps_string("gloria")

a536bb82297095d65ac6fda127e851e9c94f7424
Gloria Lavenberg <gloria@Glorias-MacBook-Air.local>
2016-06-22 21:28:24 -0400

    created files

---
+++ 1-the_basics/movies.rb
@@ -0,0 +1,6 @@
+movies = { xmen: 1975, batman: 2004, spiderman: 2013, superman: 2001, hulk: 1981 }
+puts movies [:xmen]
+puts movies [:batman]
+puts movies [:spiderman]
+puts movies [:superman]
+puts movies [:hulk]
---
+++ 1-the_basics/movies_arr.rb
@@ -0,0 +1,6 @@
+year = [1975, 2004, 2013, 2001, 1981]
+puts year [0]
+puts year [1]
+puts year [2]
+puts year [3]
+puts year [4]

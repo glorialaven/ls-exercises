ad493232ada47b1b1ca5d6574e7af6df914b57a2
Gloria Lavenberg <gloria@Glorias-MacBook-Air.local>
2016-06-23 22:44:40 -0400

    created files

---
+++ 6-arrays/arr_increm.rb
@@ -0,0 +1,4 @@
+arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+increm_arr = arr.map {|x| x + 2}
+puts arr
+puts increm_arr
---
+++ 6-arrays/check_num.rb
@@ -0,0 +1,5 @@
+arr = [1, 3, 5, 7, 9, 11]
+  if arr.include?(num)
+   puts "#{num}is included in array."
+ end
+
